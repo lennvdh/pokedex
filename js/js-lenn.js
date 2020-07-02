2f6573ca40125ae735b194b671b06019c4bc9b88
lennvdh <lenn.vanderheijden@gmail.com>
2020-07-02 14:10:37 +0200

    ()

--- js/js-lenn.js
+++ js/js-lenn.js
@@ -1,7 +1,17 @@
-let pokemon = document.getElementById('pokeinput').value;
-let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data)
-    });
+const run = document.getElementById('run');
+run.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let pokemon = document.getElementById('pokeinput').value.toLowerCase();
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            console.log(data.id);
+            console.log(data.types)
+            console.log(data.moves);
+            for(let i = 0; i<data.moves; i++){
+                console.log(data.moves[i]);
+            }
+        });
+})
